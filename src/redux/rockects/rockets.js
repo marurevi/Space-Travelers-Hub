4967748c7dfb5b8162e223ca5a100a35e9c637d5
arturo <ahpareja@gmail.com>
2022-06-14 15:34:36 -0500

    Add bookRocket action

--- src/redux/rockects/rockets.js
+++ src/redux/rockects/rockets.js
@@ -1,6 +1,7 @@
 import fetchRockets from '../../helpers/API_rockets';
 // Actions
 const SHOW_ROCKETS = 'rockets/SHOW_ROCKETS';
+const BOOK_ROCKET = 'rockets/BOOK_ROCKET';
 
 // Action Creators
 const getRockets = async (dispatch, getState) => {
@@ -11,15 +12,27 @@ const getRockets = async (dispatch, getState) => {
   }
 };
 
+const bookRockets = (id) => ({
+  type: BOOK_ROCKET,
+  payload: id,
+});
+
 // Reducer
 const initialState = [];
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case SHOW_ROCKETS:
       return action.payload;
+    case BOOK_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload) {
+          return rocket;
+        }
+        return { ...rocket, reserved: !rocket.reserved };
+      });
     default:
       return state;
   }
 }
 
-export { getRockets };
+export { getRockets, bookRockets };
