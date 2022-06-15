a47aa9784de842a2afa8a57564536b2f17e2c3f6
marurevi <maryrev@hotmail.com>
2022-06-15 13:32:02 -0300

    Add join btn functionality & fix bugs

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllMissions, joinMission } from '../redux/missions/missions';
 import '../css/Missions.css';
-import Mission from '../components/Mission';
 
 export default function Missions() {
   const dispatch = useDispatch();
@@ -11,10 +10,6 @@ export default function Missions() {
   useEffect(() => {
     dispatch(getAllMissions);
   }, []);
-
-  const onClickHandeler = (id) => {
-    dispatch(joinMission(id));
-  };
 
   return (
     <table>
@@ -27,13 +22,20 @@ export default function Missions() {
       </thead>
       <tbody>
         {missions && missions.map((mission) => (
-          <Mission
-            key={mission.id}
-            name={mission.name}
-            description={mission.description}
-            reserved={mission.reserved}
-            onClick={onClickHandeler}
-          />
+          <tr key={mission.id}>
+            <td className="name">{mission.name}</td>
+            <td className="description">{mission.description}</td>
+            <td>{mission.reserved ? 'Active Member' : 'NOT A MEMBER'}</td>
+            <td>
+              <button
+                type="button"
+                onClick={() => dispatch(joinMission(mission.id))}
+                id={mission.id}
+              >
+                {mission.reserved ? 'Leave Mission' : 'Join Mission'}
+              </button>
+            </td>
+          </tr>
         ))}
       </tbody>
     </table>
