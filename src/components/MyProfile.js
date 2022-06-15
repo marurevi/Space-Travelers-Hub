b43e5445daf8e86a4b30d84bb5a24f5cbbe4a40e
marurevi <maryrev@hotmail.com>
2022-06-15 18:14:12 -0300

    Add structure to my Profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux/es/exports';
+
 export default function MyProfile() {
+  const missions = useSelector((state) => state.reduMission) || [];
+
+  const reservedMissions = missions.filter((mission) => mission.reserved);
+
   return (
-    <p>hi</p>
+    <ul>
+      {
+        reservedMissions
+          ? reservedMissions.forEach((reserved) => <li key={reserved.id}>{reserved.name}</li>)
+          : 'There aren&apos;t missions to display'
+      }
+    </ul>
   );
 }
