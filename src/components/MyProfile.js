1c8e9025ffd675491d57de0814a79a12dd8c5867
arturo <ahpareja@gmail.com>
2022-06-15 17:44:10 -0500

    Add booked rockets to my Profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -4,13 +4,21 @@ import '../css/myProfile.css';
 
 export default function MyProfile() {
   const missions = useSelector((state) => state.reduMission) || [];
+  const rockets = useSelector((state) => state.rockets);
   const reservedMissions = missions.filter((mission) => mission.reserved);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <div className="container">
       <div className="mission-container">
         <h2>My Missions</h2>
         <ul>
           {reservedMissions.map((element) => <li key={element.id}>{element.name}</li>)}
+        </ul>
+      </div>
+      <div className="rocket-container">
+        <h2>My Rockets</h2>
+        <ul>
+          {reservedRockets.map((element) => <li key={element.id}>{element.name}</li>)}
         </ul>
       </div>
     </div>
